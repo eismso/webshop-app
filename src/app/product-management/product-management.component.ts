6fb8b6676feca6d794ce0a99cf7456b323512791
Juliane Brantner <juliane.brantner@gmx.at>
2023-01-21 15:51:50 +0100

    Product Management angelegt

---
+++ src/app/product-management/product-management.component.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../entities/product';
+
+@Component({
+    selector: 'app-product-management',
+    templateUrl: './product-management.component.html'
+})
+export class ProductManagementComponent implements OnInit {
+
+    title = '';
+    vendor = '';
+    products: Array<Product> = [];
+    selectedProduct: Product | undefined;
+
+    constructor(private http: HttpClient) {
+    }
+
+    ngOnInit(): void { }
+
+    search(): void {
+        const url = 'db.json';
+
+        const headers = new HttpHeaders()
+            .set('Accept', 'application/json');
+
+        const params = new HttpParams()
+            .set('title', this.title)
+            .set('vendor', this.vendor);
+
+        this.http
+            .get<Product[]>(url, {headers, params})
+            .subscribe({
+                next: (products: Product[]) => {
+                    this.products = products;
+                },
+                error: (errResp) => {
+                    console.error('Error loading products', errResp);
+                }
+            });
+    }
+
+    select(f: Product): void {
+        this.selectedProduct = f;
+    }
+}
