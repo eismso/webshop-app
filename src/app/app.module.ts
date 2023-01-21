065f1587b93278c17769731676d18911707d52ee
Juliane Brantner <juliane.brantner@gmx.at>
2023-01-21 17:23:55 +0100

    ProductManagement zu Components Module hinzugefügt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserManagementComponent } from './user-management/user-management.component';
+import { ProductManagementComponent } from './product-management/product-management.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserManagementComponent,
+    ProductManagementComponent
   ],
   imports: [
     BrowserModule,
