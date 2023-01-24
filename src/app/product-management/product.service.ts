5800441f774ad18ace1e6344666fe201309bb7a0
Juliane Brantner <juliane.brantner@gmx.at>
2023-01-24 20:04:54 +0100

    Fix Produktsuche

--- src/app/product-management/product.service.ts
+++ src/app/product-management/product.service.ts
@@ -1,25 +1,33 @@
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Product } from '../entities/product';
+import { Product } from 'src/app/entities/product';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
-  providedIn: 'root'
-})
+  providedIn: 'root',
+  })
+
 export class ProductService {
 
-  constructor(private http: HttpClient) { }
+  url = 'http://localhost:3000/products';
 
-  find(title: string, vendor: string): Observable<Product[]> {
-    const url = "http://localhost:3000/products";
+  constructor(private http:HttpClient) { }
+  
+  find(title: string): Observable<Product[]>{
 
-    const params = new HttpParams()
-      .set('title', title)
-      .set('vendor', vendor);
+    const headers = new HttpHeaders().set('Accept', 'application/json');
 
-    const headers = new HttpHeaders()
-      .set('accept', 'application/json');
+    const params = new HttpParams().set('title', title);
 
-    return this.http.get<Product[]>(url, { params, headers })
+    return this.http.get<Product[]>(this.url, {headers, params});
+
+  }
+
+  getCart(): Observable<Product[]>{
+    const headers = new HttpHeaders().set('Accept', 'application/json');
+
+    return this.http.get<Product[]>(this.url, {headers});
+
   }
 }
